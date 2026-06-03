e1a8d507890c544f366c6249fb6486a464db7ed4
DustySkate27 <lautarolopezutn@gmail.com>
2026-06-03 12:07:07 -0300

    Added - GameManager & Incomplete WaveManager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static Action OnWinCondition;
+
+    public static Action PreComp; //Todo lo que necesite PreComp en pantalla de carga
+
+    private void Awake()
+    {
+        OnWinCondition += StopTime;
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void ResetScene()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().ToString());
+    }
+
+    public void StopTime()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public UnityEngine.Object CreateEntity(UnityEngine.Object entity, Transform transform)
+    {
+        return Instantiate(entity, transform);
+    }
+}
---
+++ Assets/WaveManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine.Pool;
+
+
+public class WaveManager
+{
+    private ObjectPool<UnityEngine.Object> pool;
+
+    private static Action OnWaveEnd;
+
+    private int waveSize;
+    private int currentWave;
+    private int waveAmount;
+
+    public WaveManager()
+    {
+        //GameManager.PreComp += 
+    }
+
+    private void SpawnEnemy(UnityEngine.Object enemy)
+    {
+        for (int i = 0; i < waveSize; i++)
+        {
+            //pool.Spawn();
+        }
+    }
+
+    private void EnemySubstract(UnityEngine.Object enemy, float damage)
+    {
+        if (enemy.TakeDamage(damage) <= 0)
+            waveAmount--;
+
+        if (waveAmount <= 0) 
+            OnWaveEnd?.Invoke();
+    }
+
+}
