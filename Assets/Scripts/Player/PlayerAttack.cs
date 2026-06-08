bf8ae1a845094a32c353cbfc6ccf973f48c550dc
Juan-Majork <juani120805@gmail.com>
2026-06-07 21:23:20 -0300

    Fixed-Ray in Hitscan direction

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -19,10 +19,12 @@ public class PlayerAttack
 
     public void Shoot()
     {
-        Ray ray = new Ray(spawn.position, playerCamera.transform.forward);
+        Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
 
         if (Physics.Raycast(ray, out RaycastHit hit, distance, enemyLayer))
         {
+            Debug.Log($"Hit: {hit.collider.gameObject.name} en posición {hit.point}");
+
             Dictionary<UnityEngine.Object, Enemy> enemyDict = ServiceLocator.Get<Dictionary<UnityEngine.Object, Enemy>>();
 
             if (enemyDict.TryGetValue(hit.collider.gameObject, out Enemy enemy))
